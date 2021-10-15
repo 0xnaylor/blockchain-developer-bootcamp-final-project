7e0d0b4ce0abf98baf7e289aefe85e0ddc7e2566
Ben Naylor <36206067+bnaylor1989@users.noreply.github.com>
2021-10-15 13:53:19 +0100

    Truffle box installed and configured. SimpleStorgae app working in development chain and on Rinkeby.

---
+++ truffle-config.js
@@ -0,0 +1,28 @@
+const path = require("path");
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const dotenv = require("dotenv");
+
+dotenv.config();
+const mnemonic = process.env.MNEMONIC;
+
+module.exports = {
+  // See <http://truffleframework.com/docs/advanced/configuration>
+  // to customize your Truffle configuration!
+  contracts_build_directory: path.join(__dirname, "client/src/contracts"),
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 8545,
+      network_id: "*", // match any network
+      websockets: true,
+    },
+    rinkeby: {
+      provider: () => new HDWalletProvider(mnemonic, process.env.INFURA_URL),
+      network_id: 4,
+      gas: 5500000,
+      confirmations: 0, // # of confs to wait between deployments. (default: 0)
+      timeoutBlocks: 200, // # of blocks before a deployment times out  (minimum/default: 50)
+      skipDryRun: true, // Skip dry run before migrations? (default: false for public nets )
+    },
+  },
+};
